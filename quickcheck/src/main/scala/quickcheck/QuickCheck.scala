032cafc9ba8328f7ef6de82049ec0a0a9cabf779
Mike Dalton <michaelcdalton@gmail.com>
2016-08-27 20:06:46 -0400

    QuickCheck Implementing heap generator

---
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -0,0 +1,28 @@
+package quickcheck
+
+import common._
+
+import org.scalacheck._
+import Arbitrary._
+import Gen._
+import Prop._
+
+abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
+
+  lazy val genHeap: Gen[H] = for {
+    element <- arbitrary[A]
+    heap <- oneOf(const(empty), genHeap)
+  } yield insert(element, heap)
+  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+}
