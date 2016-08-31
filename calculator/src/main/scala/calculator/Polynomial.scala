612d88bef1f90d198ef74365f5074c64a457ebe1
Mike Dalton <michaelcdalton@gmail.com>
2016-08-31 18:34:32 -0400

    Calculator: implementing computeDelta and computeSolutions

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,23 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Var(
+      (b.apply() * b.apply()) -
+        (4.0 * a.apply() * c.apply())
+    )
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Var(
+      if (delta.apply() < 0) Set()
+      else {
+        Set(
+          (-1 * (b.apply()) + Math.sqrt(delta.apply())) / (2.0 * a.apply()),
+          (-1 * (b.apply()) - Math.sqrt(delta.apply())) / (2.0 * a.apply())
+        )
+      }
+
+    )
   }
 }
